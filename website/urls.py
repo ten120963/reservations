5fbaa6a8d86d57dd838089104b0ea1b918dc687c
Thomas Nowak <t.nowak@roadrunner.com>
2020-06-11 13:10:17 -0400

    added pages

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.home, name="home"),  
 	path('add/', views.add, name="add"),  
+	path('edit/<list_id>', views.edit, name="edit"),   
+	path('delete/<list_id>', views.delete, name="delete"), 
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Reservation
+from .forms import ReservationForm
+from django.contrib import messages
 
 def home(request):
 	all_reservations = Reservation.objects.all
@@ -7,4 +9,40 @@ def home(request):
 
 
 def add(request):
-	return render(request, 'add.html', {})	
+	if request.method == "POST":
+		form = ReservationForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Reservation Has Been Added!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Seems Like There Was An Error...'))	
+			return render(request, 'add.html', {})	
+	else:
+		return render(request, 'add.html', {})	
+
+def edit(request, list_id):
+	if request.method == "POST":
+		current_reservation = Reservation.objects.get(pk=list_id)
+		form = ReservationForm(request.POST or None, instance=current_reservation)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Reservation Has Been Edited!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Seems Like There Was An Error...'))	
+			return render(request, 'edit.html', {})	
+	else:
+		get_reservation = Reservation.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'get_reservation': get_reservation})
+
+def delete(request, list_id):
+	if request.method == "POST":
+		current_reservation = Reservation.objects.get(pk=list_id)
+		current_reservation.delete()
+		messages.success(request, ('Reservation Has Been Deleted!'))
+		return redirect('home')
+	else:
+		messages.success(request, ('Nothing To See Here...'))	
+		return redirect('home')			
+
