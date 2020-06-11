b6b3511658a452fdb8bcfde81e50ef89787a3891
Thomas Nowak <t.nowak@roadrunner.com>
2020-06-11 10:16:57 -0400

    modified data display

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Reservation
 
 def home(request):
-	return render(request, 'home.html', {})
+	all_reservations = Reservation.objects.all
+	return render(request, 'home.html', {'all_reservations': all_reservations})
+
 
 def add(request):
 	return render(request, 'add.html', {})	
